5cb951814c77aa664db55a798437a5fdb249b2b5
Austin Ellis <siniStar@IRC4Fun.net>
2023-07-08 22:15:58 -0500

    plugin: Fix oversigh
    -Fix oversight in botwave detection

--- plugin.py
+++ plugin.py
@@ -2465,7 +2465,7 @@ class Sigyn(callbacks.Plugin,plugins.ChannelDBHandler):
         nicks = self.registryValue('reportNicks')
         if msg.nick in nicks:
             i = self.getIrc(irc)
-            if text.startswith('DNSBL:') and not '(tor' in text:
+            if text.startswith('DNSBL:'):
                 permit = self.registryValue('reportPermit')
                 if permit > -1:
                     life = self.registryValue('reportLife')
